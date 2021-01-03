c60f120506ea7d161723a7fd2b1ec1adb64eb4ba
Shelendra Vashishtha <53626635+shelendravashishtha2@users.noreply.github.com>
2021-01-04 02:08:15 +0530

    random changes

--- lib/chatsystem/chatscreen.dart
+++ lib/chatsystem/chatscreen.dart
@@ -7,19 +7,17 @@ final _mes = FirebaseFirestore.instance;
 User currentUser;
 
 class ChatScreen extends StatefulWidget {
-   @override
+  @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
-  void getMessages()async{
-    await  for(var i in _mes.collection('message').snapshots())
-      {
-        for(var message in i.docs){
-          print(message.data());
-        }
+  void getMessages() async {
+    await for (var i in _mes.collection('message').snapshots()) {
+      for (var message in i.docs) {
+        print(message.data());
       }
+    }
   }
 
   @override
@@ -28,6 +26,7 @@ class _ChatScreenState extends State<ChatScreen> {
     super.initState();
     getMessages();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,7 +34,10 @@ class _ChatScreenState extends State<ChatScreen> {
       appBar: AppBar(
         backgroundColor: kprimaryColor,
         title: Text('BookName'),
-        leading:IconButton(icon:Icon(Icons.arrow_back_outlined) ,onPressed: (){},),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_outlined),
+          onPressed: () {},
+        ),
       ),
       body: ChatScreenBody(),
     );
@@ -43,7 +45,6 @@ class _ChatScreenState extends State<ChatScreen> {
 }
 
 class ChatScreenBody extends StatefulWidget {
-
   @override
   _ChatScreenBodyState createState() => _ChatScreenBodyState();
 }
@@ -51,7 +52,7 @@ class ChatScreenBody extends StatefulWidget {
 class _ChatScreenBodyState extends State<ChatScreenBody> {
   String message;
 
-  List<Text> messages=[];
+  List<Text> messages = [];
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +70,7 @@ class _ChatScreenBodyState extends State<ChatScreenBody> {
                   child: TextField(
                     cursorColor: Colors.white,
                     decoration: InputDecoration(
-                      filled:true,
+                      filled: true,
                       fillColor: Color(0xFF344955),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(30.0),
@@ -81,14 +82,13 @@ class _ChatScreenBodyState extends State<ChatScreenBody> {
                   ),
                 ),
                 IconButton(
-                  onPressed: ()async{
-                    try{
+                  onPressed: () async {
+                    try {
                       final messageSent = await _mes.collection('message').add({
                         'text': message,
                         'sender': 'shia.sharma123@gmail.com',
                       });
-                    }
-                    catch(e){
+                    } catch (e) {
                       final snackbar = SnackBar(content: Text('${e.message}'));
                       Scaffold.of(context).showSnackBar(snackbar);
                     }
@@ -105,23 +105,22 @@ class _ChatScreenBodyState extends State<ChatScreenBody> {
 }
 
 class MessageStream extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<QuerySnapshot>(
         stream: _mes.collection('message').snapshots(),
-        builder: (context,snapshots){
-          if(!snapshots.hasData){
+        builder: (context, snapshots) {
+          if (!snapshots.hasData) {
             return Center(
               child: CircularProgressIndicator(
                 backgroundColor: Colors.black,
               ),
             );
-          }
-          else{
+          } else {
+            print('just print');
             final message = snapshots.data.docs.reversed;
-            List<MessageBubble> messageWidgets= [];
-            for(var message in message){
+            List<MessageBubble> messageWidgets = [];
+            for (var message in message) {
               final messageText = message.data()['text'];
               final messageSender = message.data()['sender'];
               final messageWidget = MessageBubble(
@@ -134,57 +133,49 @@ class MessageStream extends StatelessWidget {
             return Expanded(
               child: ListView(
                 reverse: true,
-                padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 10.0),
+                padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 10.0),
                 children: messageWidgets,
               ),
             );
           }
-        }
-    );
+        });
   }
 }
 
-
 class MessageBubble extends StatelessWidget {
-
   final sender;
   final text;
   bool isMe;
 
-  MessageBubble({this.text,this.sender,this.isMe});
+  MessageBubble({this.text, this.sender, this.isMe});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(
-          horizontal: 10.0,
-          vertical: 20.0
-      ),
+      padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
       child: Column(
-        crossAxisAlignment:isMe? CrossAxisAlignment.end:CrossAxisAlignment.start,
+        crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
         children: [
-          Text(
-              '$sender'
-          ),
+          Text('$sender'),
           SizedBox(
             height: 5.0,
           ),
           Container(
             decoration: BoxDecoration(
-              color:isMe ?  kprimaryColor :Colors.white ,
-              borderRadius: BorderRadius.circular(30.0),
-              border: Border.all(
-                color: Colors.white,
-                width: 1,
-              )
-            ),
+                color: isMe ? kprimaryColor : Colors.white,
+                borderRadius: BorderRadius.circular(30.0),
+                border: Border.all(
+                  color: Colors.white,
+                  width: 1,
+                )),
             child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 10.0,horizontal: 10.0),
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
               child: Text(
                 '$text',
                 style: TextStyle(
                   fontSize: 15.0,
-                  color: isMe ? Colors.white:Colors.black,
+                  color: isMe ? Colors.white : Colors.black,
                 ),
               ),
             ),
